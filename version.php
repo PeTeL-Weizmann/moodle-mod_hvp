814a693cb5d1bc5edc58cb9a1dc6c5710d97b323
Nadav Kavalerchik <nadav.kavalerchik@weizmann.ac.il>
2024-08-06 14:28:26 +0300

    Update version.php
    Update to 1.26.1 with RTL support

--- version.php
+++ version.php
@@ -28,4 +28,4 @@ $plugin->requires  = 2013051403;
 $plugin->cron      = 0;
 $plugin->component = 'mod_hvp';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.23.1';
+$plugin->release   = '1.26.1';
